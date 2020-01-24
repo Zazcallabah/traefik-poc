75970133c76345596d75f66b8d166d0389d071b1
Peter Hamberg <peter.hamberg@qliro.com>
2020-01-24 17:14:02 +0100

    fix errors and add explanation

--- init.sh
+++ init.sh
@@ -1,7 +1,13 @@
+
+# init local swarm with one node
 docker swarm init
+
+# create both overlay networks
+# traefik listens on these for services to route, so use separate to avoid conflicts?
 docker network create --driver=overlay traefik-http
 docker network create --driver=overlay traefik-tcp
 
+# deploy services
 docker stack deploy traefik -c traefik.yml
 docker stack deploy traefik2 -c traefik2.yml
 docker stack deploy http -c nginx.yml
--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
-curl localhost:81 -H "Host: tcp.local"
+# if everything works, the following two lines should output "http" and "tcp" respectively
 curl localhost -H "Host: http.local"
+curl localhost:81 -H "Host: tcp.local"
